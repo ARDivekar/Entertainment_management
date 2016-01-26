eef619c756b20c2fa268f718923434a8686f02db
Abhishek Divekar <abhishek.r.divekar@gmail.com>
2016-01-26 19:51:36 +0530

    Start: defined tasks

---
+++ src/Main/Start.java
@@ -0,0 +1,21 @@
+package Main;
+
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+
+public class Start {
+	/* Tasks:
+	 * - Set up the server connection / check its connection
+	 * - Check the database exists, or else make a new database.
+	 * - Print corresponding statistics to terminal.
+	 * - Get updates for movies, TV shows, etc.
+	 *  
+	*/
+	
+	public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
+//		Document doc = Jsoup.connect("http://google.com").get();
+//		System.out.println(doc.html());
+		
+		
+	}
+}
