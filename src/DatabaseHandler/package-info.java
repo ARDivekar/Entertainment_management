9af3569db023ead7f023c4566f34eb8b7c169e29
Abhishek Divekar <abhishek.r.divekar@gmail.com>
2016-01-26 18:30:54 +0530

    First commit; package descriptions for DatabaseHandler and WebsiteExtractor

---
+++ src/DatabaseHandler/package-info.java
@@ -0,0 +1,12 @@
+/*This package handles all the database stuff:
+ * setting up the database 
+ * connecting to the database
+ * inserting into the database
+ * removing from the database
+ */
+/**
+ * @author ardivekar
+ *
+ */
+package DatabaseHandler;
+
---
+++ src/Website_Extractor/package-info.java
@@ -0,0 +1,41 @@
+/**The Website-Extractor package uses jsoup to parse the website HTML and get data and 
+ * updates/release dates for the calendar. This data is then saved in the database.
+ * 
+ * [1] Data:
+ * 	# Movies: 
+ *  	- IMDb (http://www.imdb.com/chart/top)
+ *  		Note: see side panel for genres, extract from there.
+ *  		Note 2: some movies belong to >1 genre on their movie page
+ *  
+ * 	# TV Shows:
+ * 		- IMDb (http://www.imdb.com/chart/toptv/?sort=rk,asc&mode=simple&page=1). 
+ * 			Note: see side panel for genres, extract from there.
+ * 			Note 2: some TV shows belong to >1 genre.
+ * 
+ *  # Music:
+ *  	- Apple India iTunes store (https://itunes.apple.com/in/genre/music/id34)
+ *  		Note: this is the genre list. Each genre contains artists. Each artist contains albums. Each album contains song.
+ *  		Note 2: some artists belong to >1 genre in their listing, but the artist page says only one genre.
+ * 
+ *  # Books:
+ *  	- Apple US iTunes Store (https://itunes.apple.com/us/genre/books/id38?mt=11)
+ *  		Note: this is the genre list. Each genre contains books.
+ *  		Note 2: Each book page has a 'category' section, which may not match the genre.
+ *  	- !!!!Goodreads complete genre list (http://www.goodreads.com/genres/list)
+ *  		Note: This resource is not really viable. Books outnumber Movies and TV Shows by a HUGE amount, becuase it's cheap to write a book.  
+ *  
+ *  
+ *  
+ *  
+ * [2] Release dates/updates: 
+ *  # Movies:
+ *  	- "In theatres this week": IMDb (http://www.imdb.com/movies-in-theaters/?ref_=cs_inth)
+ *  
+ * 
+ * 
+ */
+/**
+ * @author ardivekar
+ *
+ */
+package Website_Extractor;
